af8bdc3f5088a0dea04f0835c151059ca78c2dcf
Kamil_Radzimowski <kamil.radzimowski@onet.eu>
2021-12-19 23:04:58 +0100

    Project started

---
+++ read_image_header.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
+int read_bitmap_header(char* filename){
+    FILE* bitmap_file = fopen(filename, "rb");
+    return 0;
+}
