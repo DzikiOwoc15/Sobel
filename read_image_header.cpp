5fc95dec0a4fef2d5d1352d263658331ce05a520
DzikiOwoc15 <kamil.radzimowski@onet.eu>
2021-12-20 19:00:24 +0100

    completed read bitmap header

--- read_image_header.cpp
+++ read_image_header.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
-#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    read_bitmap_header("lena.bmp");
-    return 0;
+void read_bitmap_header(const char* filename){
+    FILE* f = fopen(filename, "rb");
+    char info[54];
+    string compressionType [4] = {"BI_RGB", "BI_RLE8", "BI_RLE4", "BI_BITFIELDS"};
+    fread(info, sizeof(unsigned char), 54, f);
+    int headerSize = *(int*)&info[14];
+    int width = *(int*)&info[18];
+    int height = *(int*)&info[22];
+    int planes = *(int*)&info[26];
+    int bitsPerPixel = *(int*)&info[28];
+    int compression = *(int*)&info[30];
+    int sizeImage = *(int*)&info[34];
+    int horizontalRes = *(int*)&info[38];
+    int verticalRes = *(int*)&info[42];
+    int colors = *(int*)&info[46];
+    int importantColors = *(int*)&info[50];
+    cout << "Nazwa Pliku: " << filename << endl;
+    cout << "Rozmiar naglowka: " << headerSize << endl;
+    cout << "Szerokosc: " << width << "px" << endl;
+    cout << "Wysokosc: " << height << "px" << endl;
+    cout << "Liczba platow: " <<  planes << endl;
+    cout << "Liczba bitow na pixel: " <<  bitsPerPixel << endl;
+    cout << "Rozmiar rysunku: " <<  sizeImage << endl;
+    cout << "Kompresja: " <<  compressionType[compression] <<  endl;
+    cout << "Rozdzielczosc pozioma: " <<  horizontalRes << endl;
+    cout << "Rozdzielczosc pionowa: " <<  verticalRes << endl;
+    cout << "Liczba kolorow w palecie: " <<  colors << endl;
+    cout << "Liczba waznych kolorow w palecie: " <<  importantColors << endl;
 }
 
-int read_bitmap_header(char* filename){
-    FILE* bitmap_file = fopen(filename, "rb");
-    unsigned char info[54];
-    fread(info, sizeof(unsigned char), 54, bitmap_file);
-    cout << "Name: " << filename << endl;
-    return 0;
-}
