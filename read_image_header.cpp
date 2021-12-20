45dd6bab9673f254e95a737ba96057577fe902ce
DzikiOwoc15 <kamil.radzimowski@onet.eu>
2021-12-20 16:03:22 +0100

    update

--- read_image_header.cpp
+++ read_image_header.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 int main() {
+    read_bitmap_header("lena.bmp");
     return 0;
 }
 
 int read_bitmap_header(char* filename){
     FILE* bitmap_file = fopen(filename, "rb");
+    unsigned char info[54];
+    fread(info, sizeof(unsigned char), 54, bitmap_file);
+    cout << "Name: " << filename << endl;
     return 0;
 }
