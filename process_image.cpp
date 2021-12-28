50cb190ca7fd20402e8dc78c58555f0b848a09c4
Kamil_Radzimowski <kamil.radzimowski@onet.eu>
2021-12-28 15:15:57 +0100

    new functions

--- process_image.cpp
+++ process_image.cpp
@@ -80,8 +80,41 @@ void readImageBitmapHeader(const char info[54]){
     cout << endl;
 }
 
-vector< vector< vector<int> > > applyFilter(int filterId, vector< vector< vector<int> > > data){
-    return data;
+vector< vector< vector<int> > > applyFilter(int filterId, vector< vector< vector<int> > > data, int height, int width){
+    vector< vector< vector<int> > > result(height, vector< vector<int> >(width , vector<int>(3)));
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            //If it's a border pixel set all values of that pixel to 0
+            if (i == 0 || j == 0 || i == height-1 || j == width-1){
+                result[i][j][0] = 0;
+                result[i][j][1] = 0;
+                result[i][j][2] = 0;
+            }
+            else{
+                for (int z = 0; z < 3; z++){
+                    int m0 = data[i-1][j-1][z] * filters[filterId][0][0];
+                    int m1 = data[i-1][j][z] * filters[filterId][0][1];
+                    int m2 = data[i-1][j+1][z] * filters[filterId][0][2];
+                    int m3 = data[i][j-1][z] * filters[filterId][1][0];
+                    int m4 = data[i][j][z] * filters[filterId][1][1];
+                    int m5 = data[i][j+1][z] * filters[filterId][1][2];
+                    int m6 = data[i+1][j-1][z] * filters[filterId][2][0];
+                    int m7 = data[i+1][j][z] * filters[filterId][2][1];
+                    int m8 = data[i+1][j+1][z] * filters[filterId][2][2];
+                    result[i][j][z] = m0 + m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+int calculateAverageValue(vector<vector<vector< vector<int>>>> data, int i, int j, int z){
+    int avg = 0;
+    for (int k = 0; k < 8; k++){
+        avg += data[k][i][j][z];
+    }
+    return (avg/8);
 }
 
 
@@ -99,10 +132,12 @@ void sobelImageProcessing(const char* filename, const char* newFilename){
     int bajtyZerowe = ((((bitmapSize/height)/3)) - width) * 3;
     cout << "Bajty zerowe: " << bajtyZerowe << endl;
 
- ;   int row_padded = (width*3 + 3) & (~3);
+    int row_padded = (width*3 + 3) & (~3);
     auto* data = new unsigned char[width];
 
-    vector< vector< vector<int> > > pixels(height, vector< vector<int> >(width , vector<int>(3)));
+    vector< vector< vector<int> > > pixels(height, vector< vector<int> >
+                                          (width , vector<int>
+                                          (3)));
 
     //get every pixel data
     for(int i = 0; i < height; i++){
@@ -113,13 +148,27 @@ void sobelImageProcessing(const char* filename, const char* newFilename){
             pixels[i][j][2] = (int)data[(j*3) + 2];
         }
     }
-
+    cout << "get every pixel" << endl;
+    vector<vector<vector<vector<int>>>> allFilters(8, vector<vector<vector<int>>>
+                                                  (height, vector<vector<int>>
+                                                  (width, vector<int>
+                                                  (3))));
     //apply every filter
     for (int i = 0; i < 8; i++){
-        vector<vector<vector<int>>> postFilterData =  applyFilter(i, pixels);
+        vector<vector<vector<int>>> postFilterData =  applyFilter(i, pixels, height, width);
+        allFilters[i] = postFilterData;
     }
-
+    cout << "done filters" << endl;
+    vector< vector< vector<int> > > result(height, vector< vector<int> >(width , vector<int>(3)));
     //Calculate average R G B values based on all filters
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            for (int z = 0; z < 3; z++){
+                result[i][j][z] = calculateAverageValue(allFilters, i, j, z);
+            }
+        }
+    }
+    cout << "done average" << endl;
 
     //Store the  new image in a new file
 }
